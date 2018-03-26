694caeaf9558b8a57224e3a27e9498540b6c5e1b
Ovi Fritsch <ovidiu.fritsch@gmail.com>
2018-03-25 22:17:49 -0700

    screensaver

--- canvas_script.js
+++ canvas_script.js
@@ -1,245 +1,94 @@
-var vertexShaderText = 
-[
-	'precision mediump float;',
-	'',
-	'attribute vec3 vertPosition;',
-	'attribute vec3 vertColor;',
-	'varying vec3 fragColor;',
-	'uniform mat4 mWorld;',
-	'uniform mat4 mView;',
-	'uniform mat4 mProj;',
-	'',
-	'void main()',
-	'{',
-	'	fragColor = vertColor;',
-	'	gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
-	'}'
-].join('\n');
-
-var fragmentShaderText =
-[
-'precision mediump float;',
-'',
-'varying vec3 fragColor;',
-'void main()',
-'{',
-'	gl_FragColor = vec4(fragColor, 1.0);',
-'}'
-].join('\n');
+var scene, camera, renderer;
+var table_width, table_height;
+var light;
+var white_ball;
+var flat_red_ball;
+var ball_radius = 30;
 
 
-main();
+init();
+animate();
 
-function main() {
+
+function init() {
+	table_width = $("#pool_table").width();
+	table_height = $("#pool_table").height();
+
 	var canvas = document.querySelector("#pool_table");
 	var gl = canvas.getContext("webgl");
 
-	if (!gl) {
-		alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-		return;
-	}
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.enable(gl.DEPTH_TEST);
-	gl.enable(gl.CULL_FACE);
-	gl.frontFace(gl.CCW);
-	gl.cullFace(gl.BACK);
+	scene = new THREE.Scene();
 
-	var program = initShaderProgram(gl, vertexShaderText, fragmentShaderText);
+	camera = new THREE.OrthographicCamera( table_width / - 2, table_width / 2, table_height / 2, table_height / - 2, 1, -35 );
+	renderer = new THREE.WebGLRenderer(gl, "mediump");
+	renderer.setSize(table_width, table_height);
 
-
-	//generate circle triangle indices
-	var boxVertices = 
-	[ // X, Y, Z           R, G, B
-		// Top
-		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
-		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
-		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
-		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
-
-		// Left
-		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
-		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
-		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
-		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
-
-		// Right
-		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
-		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
-		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
-		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
-
-		// Front
-		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
-		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
-
-		// Back
-		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
-		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
-
-		// Bottom
-		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
-		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
-		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
-		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
-	];
-
-	var boxIndices =
-	[
-		// Top
-		0, 1, 2,
-		0, 2, 3,
-
-		// Left
-		5, 4, 6,
-		6, 4, 7,
-
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-
-		// Bottom
-		21, 20, 22,
-		22, 20, 23
-	];
+	// light1 = new THREE.DirectionalLight(0xffffff, 0.7);
+	// light2 = new THREE.DirectionalLight(0xffffff, 0.7);
+	// light1.position.set(-0.2, 0, 0.1);
+	// light2.position.set(0.2, 0, 0.1)
+	// scene.add(light1);
+	// scene.add(light2);
 
 
+	var geometry = new THREE.SphereGeometry( ball_radius, 32, 32 );
+	var material_white = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+	//var material_flat_red = new THREE.MeshBasicMaterial( { color: "red" } );
 
+	white_ball = new THREE.Mesh( geometry, material_white );
+	//flat_red_ball = new THREE.Mesh(geometry, material_flat_red);
 
-	//do some buffer setup
-	var boxVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+	scene.add( white_ball );
+	//scene.add(flat_red_ball);
 
-	var boxIndexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
-
-	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-
-	gl.vertexAttribPointer
-	(
-		positionAttribLocation,//Attribute Loaction
-		3, //Number of elements per attribute, since its a vec3, its 3
-		gl.FLOAT, //type of elements
-		gl.FALSE, //should the data be normalized
-		6 * Float32Array.BYTES_PER_ELEMENT, //size of an individual vertex
-		0 //offset in bytes of the first component in the vertex attribute array
-	);
-
-	var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-
-	gl.vertexAttribPointer
-	(
-		colorAttribLocation,//Attribute Loaction
-		3, //Number of elements per attribute, since its a vec3, its 3
-		gl.FLOAT, //type of elements
-		gl.FALSE, //should the data be normalized
-		6 * Float32Array.BYTES_PER_ELEMENT, //size of an individual vertex
-		3 * Float32Array.BYTES_PER_ELEMENT//offset in bytes of the first component in the vertex attribute array
-	);
-
-
-	gl.enableVertexAttribArray(positionAttribLocation);
-	gl.enableVertexAttribArray(colorAttribLocation);
-
-	// Tell OpenGL state machine which program should be active
-	gl.useProgram(program);
-
-	var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
-	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
-	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
-
-	var worldMatrix = new Float32Array(16);
-	var viewMatrix = new Float32Array(16);
-	var projMatrix = new Float32Array(16);
-
-
-	mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
-	mat4.perspective(projMatrix, glMatrix.toRadian(45), 2, 0.1, 1000.0);
-
-	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
-	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-
-
-
-	var xRotationMatrix = new Float32Array(16);
-	var yRotationMatrix = new Float32Array(16);
-	var xTranslationMatrix = new Float32Array(16);
-	var interm_matrix = new Float32Array(16);
-
-
-	//draw buffer to the screen
-	var identityMatrix = new Float32Array(16);
-	mat4.identity(identityMatrix);
-	var time, angle;
-	var loop = function() {
-		time = performance.now() / 1000;
-		x_dist = (time*30 % 40) / 10;
-		angle = time / 2 * Math.PI;
-
-		
-		mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
-		mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
-		mat4.translate(xTranslationMatrix, identityMatrix, [-x_dist, 0, 0]);
-
-		mat4.mul(interm_matrix, xRotationMatrix, yRotationMatrix);
-		mat4.mul(worldMatrix, xTranslationMatrix, interm_matrix);
-		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-
-
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
-		requestAnimationFrame(loop);
-	};
-	requestAnimationFrame(loop);
 }
 
-/*
-	so this thing 
-*/
-
-function initShaderProgram(gl, vsSource, fsSource) {
-	const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderText);
-	const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderText);
-
-	const shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram, vertexShader);
-	gl.attachShader(shaderProgram, fragmentShader);
-	gl.linkProgram(shaderProgram);
-
-	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-		alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-		return null;
+var white_going_left = false;
+var white_going_down = false;
+function update_position_white() {
+	var x_speed = 5;
+	var y_speed = 5;
+	//change x direction
+	if (white_ball.position.x > table_width/2 - ball_radius) {
+		white_going_left = true;
+	}
+	else if (white_ball.position.x < -table_width/2 + ball_radius) {
+		white_going_left = false;
 	}
 
-	return shaderProgram;
-}
-
-function loadShader(gl, type, source) {
-	const shader = gl.createShader(type);
-	gl.shaderSource(shader, source);
-	gl.compileShader(shader);
-
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-		gl.deleteShader(shader);
-		return null;
+	//change y direction
+	if (white_ball.position.y > table_height/2 - ball_radius) {
+		white_going_down = true;
 	}
-	return shader;
+	else if (white_ball.position.y < -table_height/2 + ball_radius) {
+		white_going_down = false;
+	}
+
+	if (white_going_left) {
+		white_ball.position.set(white_ball.position.x - x_speed, white_ball.position.y, white_ball.position.z);
+	}
+	else {
+		white_ball.position.set(white_ball.position.x + x_speed, white_ball.position.y, white_ball.position.z);
+	}
+
+	if (white_going_down) {
+		white_ball.position.set(white_ball.position.x, white_ball.position.y - y_speed, white_ball.position.z);
+	}
+	else {
+		white_ball.position.set(white_ball.position.x, white_ball.position.y + y_speed, white_ball.position.z);
+	}
 }
 
+// var left_flat_red = false;
+// function update_position_flat_red() {
+
+// }
+
+
+
+function animate() {
+	renderer.render(scene, camera);
+	update_position_white();
+	//update_position_flat_red();
+	requestAnimationFrame( animate );
+}
