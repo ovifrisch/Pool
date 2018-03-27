9bceb43ba25c8414f7ff60158d82fca928978fed
Ovi Fritsch <ovidiu.fritsch@gmail.com>
2018-03-27 15:34:19 -0700

    accel

--- canvas_script.js
+++ canvas_script.js
@@ -1,16 +1,29 @@
 var scene, camera, renderer;
 var table_width, table_height;
 var light;
-var white_ball;
+var white_ball, white_ball_mesh;
 var flat_red_ball;
-var ball_radius = 30;
+var table;
+var ball_radius = 20;
+var trans;
 
 
 init();
+init_table();
 animate();
+
+function init_table() {
+	var geometry = new THREE.BoxGeometry( table_width, table_height, 0);
+	var material = new THREE.MeshPhongMaterial({color: "green"});
+	var mesh = new THREE.Mesh(geometry, material);
+	mesh.castShadow = true;
+	mesh.receiveShadow= true;
+	scene.add(mesh);
+}
 
 
 function init() {
+	trans = 1;
 	table_width = $("#pool_table").width();
 	table_height = $("#pool_table").height();
 
@@ -19,76 +32,96 @@ function init() {
 
 	scene = new THREE.Scene();
 
-	camera = new THREE.OrthographicCamera( table_width / - 2, table_width / 2, table_height / 2, table_height / - 2, 1, -35 );
+	//outward is negative here apparently
+	camera = new THREE.OrthographicCamera(table_width / - 2, table_width / 2, table_height / 2, table_height / - 2, -2*ball_radius, 10);
+	scene.add(camera);
+
+	//camera = new THREE.OrthographicCamera( table_width / - 2, table_width / 2, table_height / 2, table_height / - 2, ball_radius + 20, -ball_radius - 1);
 	renderer = new THREE.WebGLRenderer(gl, "mediump");
 	renderer.setSize(table_width, table_height);
+	//renderer.shadowMap.enabled = true;
 
-	// light1 = new THREE.DirectionalLight(0xffffff, 0.7);
-	// light2 = new THREE.DirectionalLight(0xffffff, 0.7);
-	// light1.position.set(-0.2, 0, 0.1);
-	// light2.position.set(0.2, 0, 0.1)
-	// scene.add(light1);
-	// scene.add(light2);
+	var light1 = new THREE.DirectionalLight( 0xffffff, 1 );
+	light1.position.set( 0, 0, 100);
+	//light1.castShadow = true;
+	scene.add(light1);
 
 
-	var geometry = new THREE.SphereGeometry( ball_radius, 32, 32 );
-	var material_white = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-	//var material_flat_red = new THREE.MeshBasicMaterial( { color: "red" } );
 
-	white_ball = new THREE.Mesh( geometry, material_white );
-	//flat_red_ball = new THREE.Mesh(geometry, material_flat_red);
+	var white_ball_geometry = new THREE.SphereGeometry( ball_radius, 32, 32 );
+	var material_white = new THREE.MeshPhongMaterial( { color: 0xffffff } );
 
-	scene.add( white_ball );
-	//scene.add(flat_red_ball);
 
+	var buffer_geometry = new THREE.BufferGeometry();
+	buffer_geometry.fromGeometry(white_ball_geometry);
+	white_ball_mesh = new THREE.Mesh( buffer_geometry, material_white );
+	// white_ball_mesh.castShadow = true;
+	// white_ball_mesh.receiveShadow = true;
+
+	white_ball_mesh.translateOnAxis(new THREE.Vector3(0, 0, 1), ball_radius);
+	scene.add(white_ball_mesh);
 }
+
+
+var transl_position = 1.0;
+var transl_acceleration = 0.001;
+
 
 var white_going_left = false;
 var white_going_down = false;
+
+
+
 function update_position_white() {
-	var x_speed = 5;
-	var y_speed = 5;
+	var middle = new THREE.Vector3();
+	var geometry = white_ball_mesh.geometry;
+
+
+	geometry.computeBoundingBox();
+
+	middle.x = (geometry.boundingBox.max.x + geometry.boundingBox.min.x) / 2;
+	middle.y = (geometry.boundingBox.max.y + geometry.boundingBox.min.y) / 2;
+	middle.z = (geometry.boundingBox.max.z + geometry.boundingBox.min.z) / 2;
+	white_ball_mesh.localToWorld(middle);
+
 	//change x direction
-	if (white_ball.position.x > table_width/2 - ball_radius) {
+	if (middle.x > table_width/2 - ball_radius) {
 		white_going_left = true;
 	}
-	else if (white_ball.position.x < -table_width/2 + ball_radius) {
+	else if (middle.x < -table_width/2 + ball_radius) {
 		white_going_left = false;
 	}
 
 	//change y direction
-	if (white_ball.position.y > table_height/2 - ball_radius) {
+	if (middle.y > table_height/2 - ball_radius){
 		white_going_down = true;
 	}
-	else if (white_ball.position.y < -table_height/2 + ball_radius) {
+	else if (middle.y < -table_height/2 + ball_radius) {
 		white_going_down = false;
 	}
 
+
+	trans += 0.01;
+
 	if (white_going_left) {
-		white_ball.position.set(white_ball.position.x - x_speed, white_ball.position.y, white_ball.position.z);
+		white_ball_mesh.translateOnAxis(new THREE.Vector3(1, 0, 0), -trans);
 	}
 	else {
-		white_ball.position.set(white_ball.position.x + x_speed, white_ball.position.y, white_ball.position.z);
+		white_ball_mesh.translateOnAxis(new THREE.Vector3(1, 0, 0), trans);
 	}
 
 	if (white_going_down) {
-		white_ball.position.set(white_ball.position.x, white_ball.position.y - y_speed, white_ball.position.z);
+		white_ball_mesh.translateOnAxis(new THREE.Vector3(0, 1, 0), -trans);
 	}
 	else {
-		white_ball.position.set(white_ball.position.x, white_ball.position.y + y_speed, white_ball.position.z);
+		white_ball_mesh.translateOnAxis(new THREE.Vector3(0, 1, 0), trans);
 	}
 }
-
-// var left_flat_red = false;
-// function update_position_flat_red() {
-
-// }
 
 
 
 function animate() {
 	renderer.render(scene, camera);
 	update_position_white();
-	//update_position_flat_red();
 	requestAnimationFrame( animate );
 }
